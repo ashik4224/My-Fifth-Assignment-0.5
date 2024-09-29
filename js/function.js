4d398f86fdd44127398c2ce4c4de6f65a9725951
ashik4224 <ashik426370@gmail.com>
2024-09-29 23:36:15 +0000

    add my js function   section

---
+++ js/function.js
@@ -0,0 +1,16 @@
+// All input value get by ID 
+function inputFieldGetById (id){
+    const inputValue = Number(document.getElementById(id).value);
+    return inputValue;
+}
+// hide section and show section when click button
+function showSectionById (id){
+    document.getElementById('donation-content').classList.add('hidden')
+    document.getElementById('history-section').classList.add('hidden')
+
+    document.getElementById(id).classList.remove('hidden')
+}
+
+
+
+
